686e6a9c5a085bb3918e8bf00a10e888c9c1e279
maaztausif <maaztausif48@gmail.com>
2025-07-19 03:09:16 +0200

    Connection issue 80% resolved

---
+++ src/main/java/com/maaz/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.maaz;
+
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    public void service(HttpServletRequest req, HttpServletResponse res){
+        System.out.println("In Service ");
+    }
+}
